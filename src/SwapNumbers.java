6c0e70d37fac2ccc661e0ab0599b7a09e16ba925
komal.bhatt <komal18@navgurukul.org>
2019-05-30 14:49:28 +0530

    Add.copy numbers.

--- src/SwapNumbers.java
+++ src/SwapNumbers.java
@@ -1,16 +1,21 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+
+import static java.util.Collections.copy;
 
 public class SwapNumbers {
     public static void main(String[] args) {
-        System.out.println(getNumbers(Arrays.asList(12,13,14,19),0,1));
-
+        System.out.println(getNumbers(Arrays.asList(12,13,14,19),1,3));
+        System.out.println(getNumbers(Arrays.asList(12,13,14,1),1,3));
 
     }
     public static List<Integer> getNumbers(List<Integer> numbers, int first, int second){
-        int numberAtFirstIndex = numbers.get(first);
-        numbers.set(first,numbers.get(second));
-        numbers.set(second,numberAtFirstIndex);
-        return numbers;
+        List<Integer> numbersCopy = new ArrayList<>(numbers);
+        int numberAtFirstIndex = numbersCopy.get(first);
+        numbersCopy.set(first,numbersCopy.get(second));
+        numbersCopy.set(second,numberAtFirstIndex);
+        return numbersCopy;
     }
 }
