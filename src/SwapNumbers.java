a6b8f67cf4bc980693b661104a77eabbfe68821d
komal.bhatt <komal18@navgurukul.org>
2019-05-30 14:56:02 +0530

    Add.swapString.

--- src/SwapNumbers.java
+++ src/SwapNumbers.java
@@ -1,21 +1,27 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-
-import static java.util.Collections.copy;
 
 public class SwapNumbers {
     public static void main(String[] args) {
-        System.out.println(getNumbers(Arrays.asList(12,13,14,19),1,3));
-        System.out.println(getNumbers(Arrays.asList(12,13,14,1),1,3));
+        System.out.println(swapNumbers(Arrays.asList(12, 13, 14, 19), 1, 3));
+        System.out.println(swapStrings(Arrays.asList("komal", "pooja", "ankita"), 1, 2));
 
     }
-    public static List<Integer> getNumbers(List<Integer> numbers, int first, int second){
+
+    public static List<Integer> swapNumbers(List<Integer> numbers, int first, int second) {
         List<Integer> numbersCopy = new ArrayList<>(numbers);
         int numberAtFirstIndex = numbersCopy.get(first);
-        numbersCopy.set(first,numbersCopy.get(second));
-        numbersCopy.set(second,numberAtFirstIndex);
+        numbersCopy.set(first, numbersCopy.get(second));
+        numbersCopy.set(second, numberAtFirstIndex);
         return numbersCopy;
+    }
+
+    public static List<String> swapStrings(List<String> strings, int first, int second) {
+        List<String> stringCopy = new ArrayList<>(strings);
+        String stringAtFirstIndex = stringCopy.get(first);
+        stringCopy.set(first, stringCopy.get(second));
+        stringCopy.set(second, stringAtFirstIndex);
+        return stringCopy;
     }
 }
